5c69f312ee102ccf0b2f2d57087179de81c7c987
nicole-cayambas <55810654+nicole-cayambas@users.noreply.github.com>
2022-08-29 01:06:07 +0800

    added a user in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'username'=> 'niks',
+            'email' => 'niks@test.com',
+            'password' => Hash::make('secret')
+        ]);
     }
 }
